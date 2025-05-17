e8150d1beaa5b3eb87483687ba3a5b3a049148bb
tomo0611 <tomo0611@hotmail.com>
2025-05-17 20:13:12 +0900

    add: Cardで囲んでOutlineにした

--- src/components/CurriculumSelector.vue
+++ src/components/CurriculumSelector.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class="d-flex flex-sm-row flex-column justify-center align-center ga-4 mt-4">
-        <v-select label="入学年度" :items="years" v-model="defaultYear" style="max-width: 120px; min-width: 120px;" dense
-            hide-details></v-select>
-        <v-select label="入学学期" :items="semesters" v-model="defaultSemester" style="max-width: 120px; min-width: 120px;"
-            dense hide-details></v-select>
-        <v-select label="教育課程" :items="curriculums" v-model="defaultCurriculum"
-            style="max-width: 240px; min-width: 120px;" dense hide-details></v-select>
-    </div>
+    <h3 class="text-h5 text-center mt-8">教育課程指定</h3>
+    <v-card class="mx-auto w-sm-50 pa-4 mt-2" variant="outlined">
+        <div class="d-flex flex-sm-row flex-column justify-center align-center ga-4 mt-4">
+            <v-select label="入学年度" :items="years" v-model="defaultYear" style="max-width: 120px; min-width: 120px;"
+                dense hide-details></v-select>
+            <v-select label="入学学期" :items="semesters" v-model="defaultSemester"
+                style="max-width: 120px; min-width: 120px;" dense hide-details></v-select>
+            <v-select label="学科組織" :items="curriculums" v-model="defaultCurriculum"
+                style="max-width: 240px; min-width: 120px;" dense hide-details></v-select>
+        </div>
+    </v-card>
 </template>
 
 <script>
