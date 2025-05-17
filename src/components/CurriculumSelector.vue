3d20fb1cd6e962ac630b1ce17b38904fccf7dbd9
tomo0611 <tomo0611@hotmail.com>
2025-05-17 21:29:33 +0900

    fix: 課程選択の横幅を増やした

--- src/components/CurriculumSelector.vue
+++ src/components/CurriculumSelector.vue
@@ -1,6 +1,6 @@
 <template>
     <h3 class="text-h5 text-center mt-8">教育課程指定</h3>
-    <v-card class="mx-auto w-sm-50 pa-4 mt-2" variant="outlined">
+    <v-card class="mx-auto w-sm-75 pa-4 mt-2" variant="outlined" rounded="lg">
         <div class="d-flex flex-sm-row flex-column justify-center align-center ga-4 mt-4">
             <v-select label="入学年度" :items="years" v-model="defaultYear" style="max-width: 120px; min-width: 120px;"
                 dense hide-details></v-select>
