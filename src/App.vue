708a86457135adc91223415be3541afdd749991e
tomo0611 <tomo0611@hotmail.com>
2025-05-17 20:48:58 +0900

    fix: 下が狭すぎたので修正

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
   <v-app>
     <Header />
     <v-main class="background">
+      <Introduction />
       <CurriculumSelector />
       <SubjectList />
-      <HelloWorld />
+      <div class="mt-12" />
     </v-main>
     <Footer />
   </v-app>
