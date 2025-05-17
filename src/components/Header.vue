b892c81528864df37287ab139c704cf82ef9ac86
tomo0611 <tomo0611@hotmail.com>
2025-05-17 23:04:16 +0900

    change: ヘッダの名前変更

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
     <v-app-bar :elevation="2" color="primary">
-        <v-app-bar-title>ぼくたちは卒業ができない</v-app-bar-title>
+        <v-app-bar-title class="text-white">大阪公立のジュリエット</v-app-bar-title>
     </v-app-bar>
 </template>
