8e2db527628cb00f18b1e93ef771dd5eee3dc424
tomo0611 <tomo0611@hotmail.com>
2025-05-17 20:00:08 +0900

    add: テーマを追加

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -14,6 +14,16 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'light',
+    defaultTheme: 'Theme',
+    themes: {
+      Theme: {
+        dark: false,
+        colors: {
+          primary: '#5C74BC',
+          'primary-lighter': '#d7dff5',
+          secondary: '#F7C6D0',
+        },
+      },
+    },
   },
 })
